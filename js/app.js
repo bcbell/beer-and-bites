762ea2b773fd3893239171b784272eac077538c7
bcbell <coleman.britt@gmail.com>
2020-11-19 19:49:50 -0500

    Update: Both arrays within functions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
-// document.addEventListener('DOMContentLoaded', ()=>{
-  /*-------------Variables----------------*/
+/*-------------Variables----------------*/
   const beer= []
-  let str = "Too many request. Please try again later"
+  let str = ""
 
   /*------------Cached Element References------*/
+ //Buttons
   const beerPairBtn = document.getElementById('beerPairBtn')
   const brewerySearch = document.getElementById('brewerysearchBtn')
+  //Areas
+  const breweryLocatorInfo=document.getElementById('brewerylocator')
   
 
 //Beer & Pairings
     fetch("https://api.punkapi.com/v2/beers/random")
-    .then((response)=>console.log(response.json()))
-    .then ( data => {
-        console.log(data)//change to return
-
-
+    .then (function(response){
+        console.log (response.json())
     })
+    .then (data => {
+        console.log(data)//change to return
+        for(beerFood in data){
+            const beerPairings= new Array(data[beerFood].name, data[beerFood].tagline, data[beerFood].description, data[beerFood].ingredients, data[beerFood].food_pairing, data[beerFood].brewers_tips)
+            showMeBeer(beerPairings)
+        }
+        beerPairings.innerHTML= str
+     })
 
 
 //Brewery Search
     fetch("https://api.openbrewerydb.org/breweries/search?query=dog")
-    .then ((response)=> console.log(response.json()))
+    .then (function(response){
+        console.log (response.json())
+    })
     .then (data => {
         console.log(data) // change to return
+       for(beer in data){
+           const beerLocator= new Array(data[beer].name, data[beer].brewery_type,data[data].street, data[beer].city,data[beer].state, data[beer].postal.code, data[beer].country, data[beer].phone,data[beer].website_url)
+           showMeBeer(beerLocator)
+       }
+       breweryLocatorInfo.innerHTML= str
     })
 
+    function showMeBeer(beer){
+        console.log(beer)
+    }
 
 
 
@@ -36,29 +53,5 @@
 
 
 
-    // beerBtn.addEventListener('click', ()=> {
-    //     fetch("https://api.punkapi.com/v2/beers/random")
-    //        .then((response) => {
-    //            console.log(response.json())
-    //            return response.json.text
-    //        })
-    //        .then((data =>{
-    //            console.log(data.json())
-    //            document.getElementById('beerpairings')
-    //        }
-       
-           // let beerItems = () => {
-           //     const name = data[0].name
-           //     const tagline = data[0].tagline
-           //     const description = data[0].description
-           //     const { ingredients } = data[0] //ingredients is an object and the following are objects within an object
-           //     const malt = ingredients.malt
-           //     const hops = ingredients.hops
-           //     const food_pairing = data[0].food_pairing
-           //     const brewers_tips = data[0].brewers_tips
-               
-           //     randomBeerInfo.innerHTML=parseInt[name, tagline, description, malt, hops, food_pairing, brewers_tips]}
 
-//     }
-
-// }
+   
