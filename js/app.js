1666afefbdec22dde65d0c97b4c93bb2cfff892d
bcbell <coleman.britt@gmail.com>
2020-11-19 23:07:30 -0500

    Update: My buttons are connected and retrieving data from the APIs. Working on getting information displayed in the selected fields

--- js/app.js
+++ js/app.js
@@ -1,57 +1,84 @@
 /*-------------Variables----------------*/
-  const beer= []
-  let str = ""
+  const beers= []
 
   /*------------Cached Element References------*/
  //Buttons
   const beerPairBtn = document.getElementById('beerPairBtn')
   const brewerySearch = document.getElementById('brewerysearchBtn')
-  //Areas
-  const breweryLocatorInfo=document.getElementById('brewerylocator')
-  
+ 
 
-//Beer & Pairings
-    fetch("https://api.punkapi.com/v2/beers/random")
+//Beer & Pairings 
+beerPairBtn.addEventListener('click', ()=>{
+
+fetch("https://api.punkapi.com/v2/beers/random")
     .then (function(response){
-        console.log (response.json())
+        return response.json()
     })
     .then (data => {
         console.log(data)//change to return
-        for(beerFood in data){
-            const beerPairings= new Array(data[beerFood].name, data[beerFood].tagline, data[beerFood].description, data[beerFood].ingredients, data[beerFood].food_pairing, data[beerFood].brewers_tips)
-            showMeBeer(beerPairings)
-        }
-        beerPairings.innerHTML= str
+           for(beer in beers){ //loops through the property of an object
+           const name = data[0].name
+           console.log(name)
+           const tagline= data[0].tagline
+           console.log(tagline)
+            const description = data[0].description
+            console.log(description)
+            const image_url = data[0].image_url
+            console.log (image_url)
+            const {ingredients} = data[0] //ingredients is an object and the following are objects within an object
+            const malt = ingredients.malt
+            console.log(malt)
+            const hops = ingredients.hops
+            console.log(hops)
+            const food_pairing = data[0].food_pairing
+            console.log(food_pairing)
+            const brewers_tips = data[0].brewers_tips
+            console.log(brewers_tips)
+            
+             
+        document.getElementById('beerPair').innerHTML += beers
+            }
+                
+
+        })
      })
 
-
-//Brewery Search
-    fetch("https://api.openbrewerydb.org/breweries/search?query=dog")
-    .then (function(response){
-        console.log (response.json())
-    })
-    .then (data => {
-        console.log(data) // change to return
-       for(beer in data){
-           const beerLocator= new Array(data[beer].name, data[beer].brewery_type,data[data].street, data[beer].city,data[beer].state, data[beer].postal.code, data[beer].country, data[beer].phone,data[beer].website_url)
-           showMeBeer(beerLocator)
-       }
-       breweryLocatorInfo.innerHTML= str
-    })
-
-    function showMeBeer(beer){
-        console.log(beer)
-    }
-
-
-
-
-
-
-
-
-
-
-
-
    
+//Brewery Search
+
+brewerysearchBtn.addEventListener('click', ()=>{
+
+    fetch("https://api.openbrewerydb.org/breweries/")
+        .then (function(response){
+            return response.json()
+        })
+        .then (data => {
+            console.log(data)//change to return
+               for(beer in beers){ //loops through the property of an object
+                const name = data.name
+                console.log(name)
+                 const type = data.brewery_type
+                 console.log(type)
+                 const street =data.street
+                 console.log(street) 
+                 const city = data.city
+                 console.log (city)
+                 const state= data.state
+                 console.log(state)
+                 const zip= data.postal_code
+                 console.log(zip)
+                 const country= data.country
+                 console.log(country) 
+                 const phone= data.phone
+                 console.log(phone)
+                 const website= data.website_url
+                 console.log(website)
+                
+                 
+            document.getElementById('beerPair').innerHTML += beers
+                }
+                    
+    
+            })
+         })
+    
