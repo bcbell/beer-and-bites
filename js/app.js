8025cf4a93e8ecfda50f40ee9232aae28fc87796
bcbell <coleman.britt@gmail.com>
2020-11-30 23:21:19 -0500

    Updated: styling

--- js/app.js
+++ js/app.js
@@ -29,28 +29,21 @@ fetch("https://api.punkapi.com/v2/beers/random")
     .then (data => {
         console.log (data)//change to return
            for(beers in data){ //loops through the property of an object
-           const name = data[beers].name
-           console.log(name)
-           const tagline= data[beers].tagline
-           console.log(tagline)
+            const name = data[beers].name
+            const tagline= data[beers].tagline
             const description = data[beers].description
-            console.log(description)
             const image_url = data[beers].image_url
-            console.log (image_url)
             const ingredients = data[beers].ingredients //ingredients is an object and the following are objects within an object
             const malt = ingredients.malt
-            console.log(malt)
             const hops = ingredients.hops
-            console.log(hops)
             const food_pairing = data[beers].food_pairing
-            console.log(food_pairing)
             const brewers_tips = data[beers].brewers_tips
-            console.log(brewers_tips)
+        
           
            displayBP.innerHTML = `<p><h5 class= "name"> ${name}</h5></p>
-           <h4 class= "tagline"> ${tagline} </h4></p>
-           <img class= "beerImage" src="${image_url}"/>
-           <p class= "beerDes"><h4 class="beerDesTitle"> Beer Description:</h4>${description}</p><p class= "brewT"><h4 class ="brewTips"> Brew Tips:</h4> ${brewers_tips}</p><p class ="foodOptions"><h4 class ="foodOptionsTitle">Food Pairing Options:</h4><h5 class= "food"> ${food_pairing}</h5></p>`   
+                <h4 class= "tagline"> ${tagline} </h4></p>
+                <img class= "beerImage" src="${image_url}"/>
+                <p class= "beerDes"><h4 class="beerDesTitle"> Beer Description:</h4>${description}</p><p class= "brewT"><h4 class ="brewTips"> Brew Tips:</h4> ${brewers_tips}</p><p class ="foodOptions"><h4 class ="foodOptionsTitle">Food Pairing Options:</h4><h5 class= "food"> ${food_pairing}</h5></p>`   
         }
 
     })   
@@ -70,11 +63,10 @@ triviaBtn.addEventListener('click', ()=>{
         console.log(data)//change to return
         for(beers in data){  
             const question =data[beers].question
-            console.log(question)
             const answer= data[beers].answer
-            console.log(answer)
            
-            displayT.innerHTML=`<p class= "question"><h3 class= questionAnswer> Question:</h3></p><p>${question}</p><button id= "answerBtn"> Answer</button><p class= "trivia_answer">${answer}</p>`
+            displayT.innerHTML=`<p class= "question"><h3 class= questionAnswer> Question:</h3></p>
+                <p>${question}</p><button id= "answerBtn"> Answer</button><p class= "trivia_answer">${answer}</p>`
             }  
 })   
             .catch((err)=>{
@@ -95,27 +87,20 @@ brewerySearchBtn.addEventListener('click', (evt)=>{
         .then (data => {
             console.log(data)//change to return
             for(beers in data){ 
-                const name = data[beers].name
-                console.log(name)
+                 const name = data[beers].name
                  const type = data[beers].brewery_type
-                 console.log(type)
                  const street =data[beers].street
-                 console.log(street) 
                  const city = data[beers].city
-                 console.log (city)
                  const state= data[beers].state
-                 console.log(state)
                  const zip= data[beers].postal_code
-                 console.log(zip)
                  const country= data[beers].country
-                 console.log(country) 
                  const phone= data[beers].phone
-                 console.log(phone)
                  const website= data[beers].website_url
-                 console.log(website)
+                 
              
                 
-                displayBL.innerHTML =`<p><h4>${name}</h4></p><p>Address: ${street}</p><p>${city}, ${state} ${zip} ${country}</p><p>Phone: ${phone}</p><p>Website : ${website}</p>`
+                displayBL.innerHTML =`<p><h4>${name}</h4></p><p>Address: ${street}</p><p>${city}, 
+                    ${state} ${zip} ${country}</p><p>Phone: ${phone}</p><p>Website : ${website}</p>`
                 
                  
             }
@@ -139,12 +124,12 @@ recipeSearchBtn.addEventListener('click', (evt)=>{
             for(beers in data){ 
                 const {base_layer} = data.base_layer
                 const name = data.name
-                console.log(name)
                 const recipe = data.recipe
-                console.log(recipe)
+                
 
 
-                displayR.innerHTML =`<p><h4>${name}</h4></p><p id= "recipeInfo"><h5>${recipe}</h5></p>`        
+                displayR.innerHTML =`<p><h4>${name}</h4></p><p id= "recipeInfo"><h5>${recipe}</h5>
+                    </p>`        
                     
                 }
             })     
